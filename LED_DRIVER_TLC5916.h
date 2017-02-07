2099dcf8d691a9298e2f5df05b85a62b3dc13d3c
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-02-07 16:21:11 +0530

    added code to control tlc5916. also added a platform independent layer to the library so that it can easily be used with any mcu architecture

--- LED_DRIVER_TLC5916.h
+++ LED_DRIVER_TLC5916.h
@@ -1,36 +1,44 @@
 //////////////////////////////////////////////////////////////////
-// LED DRIVER - TLC5926
+// LED DRIVER - TLC5916
 //
-// LIBRARY FOR 16 CHANNEL CONSTANT CURRENT SINK TLC5926 FROM
+// LIBRARY FOR 8 CHANNEL CONSTANT CURRENT SINK TLC5916 FROM
 // TEXAS INSTRUMENTS
 //
-// NOTE : DEPENDS ON AVR_GPIO LIBRARY
+// NOTE : DEPENDS ON GPIO LIBRARY. GPIO LIBRARY WOULD BE PLATFORM
+//		  DEPENDENT (AVR/ARM/ESP)
 //
-// JANUARY 6, 2017
+// FEBRUARY 6, 2017
 //
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
 //////////////////////////////////////////////////////////////////
 
-#ifndef _LED_DRIVER_TLC5926_H_
-#define _LED_DRIVER_TLC5926_H_
+#ifndef _LED_DRIVER_TLC5916_H_
+#define _LED_DRIVER_TLC5916_H_
 
-#include "../AVR_GPIO/AVR_GPIO.h"
+#include <osapi.h>
+#include <ets_sys.h>
+#include <gpio.h>
 
 //END USER DEFINABLE LIBRARY PARAMETERS/////////////////
-#define LED_DRIVER_TLC5926_GPIO_SDI
-#define LED_DRIVER_TLC5926_GPIO_CLK
-#define LED_DRIVER_TLC5926_GPIO_LE
-#define LED_DRIVER_TLC5926_GPIO_OE	
+#define LED_DRIVER_TLC5916_GPIO_SDI		(5)
+#define LED_DRIVER_TLC5916_GPIO_CLK		(5)
+#define LED_DRIVER_TLC5916_GPIO_LE		(5)
+#define LED_DRIVER_TLC5916_GPIO_OE		(5)
 
-#define LED_DRIVER_TLC5926_NUM				(1)
-#define LED_DRIVER_TLC5926_CLK_DELAY_MS		(100)
-#define LED_DRIVER_TLC5926_HAS_OE_CONTROL	(0)
+#define LED_DRIVER_TLC5916_NUM				(1)
+#define LED_DRIVER_TLC5916_CLK_DELAY_MS		(100)
+#define LED_DRIVER_TLC5916_HAS_OE_CONTROL	(0)
 
-void LED_DRIVER_TLC5926_Init(void);
-void LED_DRIVER_TLC5926_Turn_On(void);
-void LED_DRIVER_TLC5926_Turn_Off(void);
-void LED_DRIVER_TLC5926_Send_Value(uint16_t* val);
+//INTERNAL FUNCTIONS/////////////////
+static GPIO_HIGH(uint8_t gpio_num);
+static GPIO_LOW(uint8_t gpio_num);
+static SYS_DELAY_MS(uint16_t delay_val);
+
+void LED_DRIVER_TLC5916_Init(void);
+void LED_DRIVER_TLC5916_Turn_On(void);
+void LED_DRIVER_TLC5916_Turn_Off(void);
+void LED_DRIVER_TLC5916_Send_Value(uint8_t* val);
 
 
 #endif
